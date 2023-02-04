dd89ea52fdb4c86d6c222b1b2a38b9daebaa9f60
jassiG <jaswant_s@ch.iitr.ac.in>
2023-02-04 09:18:47 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import './style.css';
+import Main from './source/main';
+
+export default function App() {
+  return <div>{Main()}</div>;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-rkumms
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-rkumms)
